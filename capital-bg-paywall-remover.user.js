59d0b266027ca5275dfab03a671c08777deedaa5
Kiril Mitov <knmitov@gmail.com>
2024-02-22 02:07:14 +0200

    update version tag

--- capital-bg-paywall-remover.user.js
+++ capital-bg-paywall-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Capital.bg Paywall Remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Capital.bg Paywall Remover
 // @author       Kiril Mitov
 // @match        https://www.capital.bg/*
